5646ba71a1fb08882debf4c8921617c388dce312
brazooka <lgvier@gmail.com>
2020-04-03 01:57:47 -0400

    minor tweaks

--- bin/upgrade-arch.sh
+++ bin/upgrade-arch.sh
@@ -7,7 +7,7 @@ yay
 yay -Yc
 
 rustup update
-for i in $(pip list -o | awk 'NR > 2 {print $1}'); do sudo pip install -U $i; done
+#for i in $(pip list -o | awk 'NR > 2 {print $1}'); do sudo pip install -U $i; done
 
 cd ~/dotfiles
 git pull
