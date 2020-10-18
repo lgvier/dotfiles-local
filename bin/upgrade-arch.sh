62eb9fc91c8eda1c782af5f3e0bb9c39ed83680c
geovani <lgvier@gmail.com>
2020-10-18 19:52:41 -0400

    minor tweaks

--- bin/upgrade-arch.sh
+++ bin/upgrade-arch.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 set -e
 
+echo pacman
 sudo pacman -Syu
+
+echo yay
 yay
 # clean unneeded dependencies
 yay -Yc
 
+echo rust
 rustup update
 #for i in $(pip list -o | awk 'NR > 2 {print $1}'); do sudo pip install -U $i; done
 
+echo dotfiles
 cd ~/dotfiles
 git pull
 
+echo dotfiles-local
 cd ~/dotfiles-local
 git pull
 
+echo rcup
 rcup
